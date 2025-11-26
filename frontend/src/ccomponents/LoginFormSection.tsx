52a07af75fb87f5bd0b377f6648cc725b109558e
songye38 <ddev0308@gmail.com>
2025-11-26 18:17:22 +0900

    UPDATE 로그인 기능 연결

--- frontend/src/ccomponents/LoginFormSection.tsx
+++ frontend/src/ccomponents/LoginFormSection.tsx
@@ -4,11 +4,47 @@ import { useState } from "react";
 import logoIcon from '/common/logo.png'
 import { COLORS } from "../types/Colors";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import type { CSSProperties } from "react";
+import { ClipLoader } from "react-spinners";
+import { loginUser } from "../api/auth";
+
+const loaderStyle: CSSProperties = {
+    display: "block",
+    margin: "0 auto",
+};
 
 export default function LoginFormSection() {
     const navigate = useNavigate();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [_error, setError] = useState("");
+    const [_success, setSuccess] = useState("");
+    const [loading, setLoading] = useState(false); // 🔹 로딩 상태 추가
+
+
+    const handleSubmit = async () => {
+        setError("");
+        setSuccess("");
+        setLoading(true); // 🔹 API 요청 시작 시 로딩 true
+
+        try {
+            await loginUser({
+                email,
+                password,
+            });
+            setSuccess("로그인 성공 🎉");
+            toast.success("로그인 성공 🎉");
+            navigate('/')
+
+        } catch (err) {
+            if (err instanceof Error) setError(err.message);
+            else setError("로그인 실패.");
+            toast.error("로그인 실패");
+        } finally {
+            setLoading(false); // 🔹 API 요청 끝나면 항상 로딩 false
+        }
+    };
 
     return (
         <div style={{ display: "flex", flexDirection: "column", gap: 16, backgroundColor: COLORS.primary[100], borderRadius: 16, padding: "28px 20px" }}>
@@ -30,7 +66,11 @@ export default function LoginFormSection() {
                     <div style={{ padding: 10, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex' }}>
                         <div style={{ color: '#ED1515', fontSize: 14, fontFamily: 'Pretendard', fontWeight: '600', wordWrap: 'break-word' }}>비밀번호가 일치하지 않습니다.</div>
                     </div>
-                    <MainBtn>로그인</MainBtn>
+                    {/* 🔹 로딩 스피너 */}
+                    {loading && <ClipLoader color="#455CC5" loading={loading} cssOverride={loaderStyle} size={40} />}
+                    <MainBtn onClick={handleSubmit} disabled={loading}>
+                        {loading ? "로그인 중..." : "로그인"}
+                    </MainBtn>
                 </div>
 
             </div>
