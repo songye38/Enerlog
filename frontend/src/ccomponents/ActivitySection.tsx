7d03422f9e127157b564dbf3a56c696725bc4ff6
songye38 <ddev0308@gmail.com>
2025-11-28 13:31:44 +0900

    UPDATE menuIcon 아래 menuOpen메뉴가 나오도록 수정

--- frontend/src/ccomponents/ActivitySection.tsx
+++ frontend/src/ccomponents/ActivitySection.tsx
@@ -73,122 +73,131 @@ export default function ActivitySection({ activity }: ActivitySectionProps) {
             에너지 레벨 : {activity.level}
           </div>
         </div>
-        <div style={{ flex: "1 1 0", height: 8 }} />
-        {/* 여기에 아이콘 예시 */}
-        <img src={heartIcon} alt="Heart Icon" style={{ width: 16, height: 16 }} />
-        <img
-          src={menuIcon}
-          alt="Menu Icon"
-          style={{ width: 20, height: 20, cursor: "pointer" }}
-          onClick={handleMenuClick}
-        />
-      </div>
-      {/* 메뉴 오픈 시 띄우기 */}
-      {menuOpen && (
-        <div style={{ position: "absolute", top: 28, right: 0, zIndex: 50 }}>
-          <UpdateMenu onNavigate={handleNavigate} />
-        </div>
-      )}
+        <div style={{ display: "flex", alignItems: "center", gap: 8 }}>
+          <div style={{ flex: "1 1 0", height: 8 }} />
 
-      {/* 활동 내용 */}
-      <div
-        style={{
-          alignSelf: "stretch",
-          display: "flex",
-          flexDirection: "column",
-          gap: 8,
-        }}
-      >
-        {/* 상단 태그 */}
+          {/* 하트 아이콘 */}
+          <img src={heartIcon} alt="Heart Icon" style={{ width: 16, height: 16 }} />
+
+          {/* 메뉴 아이콘 */}
+          <div style={{ position: "relative" }}>
+            <img
+              src={menuIcon}
+              alt="Menu Icon"
+              style={{ width: 20, height: 20, cursor: "pointer" }}
+              onClick={handleMenuClick}
+            />
+
+            {/* menuIcon 바로 아래에 뜨는 메뉴 */}
+            {menuOpen && (
+              <div style={{ marginTop: 8 }}>
+                <UpdateMenu onNavigate={handleNavigate} />
+              </div>
+            )}
+          </div>
+        </div>
+
+
+
+        {/* 활동 내용 */}
         <div
           style={{
+            alignSelf: "stretch",
             display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: 7,
             flexDirection: "column",
+            gap: 8,
           }}
         >
+          {/* 상단 태그 */}
           <div
             style={{
-              padding: "5px 8px",
-              background: "black",
-              borderRadius: 4,
-              display: "inline-flex",
+              display: "flex",
               justifyContent: "center",
               alignItems: "center",
-              gap: 4,
+              gap: 7,
+              flexDirection: "column",
             }}
           >
             <div
               style={{
-                color: "white",
-                fontSize: 12,
-                fontFamily: "Pretendard",
-                fontWeight: 600,
-                wordWrap: "break-word",
+                padding: "5px 8px",
+                background: "black",
+                borderRadius: 4,
+                display: "inline-flex",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: 4,
               }}
             >
-              #{activity.count}회 수행
-            </div>
-          </div>
-
-          {/* 제목 */}
-          <div style={{ alignSelf: "stretch", display: "flex", justifyContent: "center" }}>
-            <div
-              style={{
-                color: "black",
-                fontSize: 25,
-                fontFamily: "IsYun, sans-serif",
-                fontWeight: 400,
-                lineHeight: "24px",
-                textAlign: "center",
-              }}
-            >
-              {activity.title}
-            </div>
-          </div>
-        </div>
-
-        {/* 설명 및 태그 */}
-        <div style={{ display: "flex", flexDirection: "column", gap: 8, alignItems: "center" }}>
-          <div style={{ textAlign: "center", color: "black", fontSize: 15, lineHeight: '22px' }}>
-            {activity.description.split("\n").map((line, i) => (
-              <span key={i}>
-                {line}
-                <br />
-              </span>
-            ))}
-          </div>
-
-          {/* 해시태그 */}
-          <div style={{ display: "flex", gap: 8, flexWrap: "wrap", justifyContent: "center" }}>
-            {activity.tags.map((tag, i) => (
               <div
-                key={i}
                 style={{
-                  borderRadius: 4,
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  padding: "2px 6px",
+                  color: "white",
+                  fontSize: 12,
+                  fontFamily: "Pretendard",
+                  fontWeight: 600,
+                  wordWrap: "break-word",
                 }}
               >
+                #{activity.count}회 수행
+              </div>
+            </div>
+
+            {/* 제목 */}
+            <div style={{ alignSelf: "stretch", display: "flex", justifyContent: "center" }}>
+              <div
+                style={{
+                  color: "black",
+                  fontSize: 25,
+                  fontFamily: "IsYun, sans-serif",
+                  fontWeight: 400,
+                  lineHeight: "24px",
+                  textAlign: "center",
+                }}
+              >
+                {activity.title}
+              </div>
+            </div>
+          </div>
+
+          {/* 설명 및 태그 */}
+          <div style={{ display: "flex", flexDirection: "column", gap: 8, alignItems: "center" }}>
+            <div style={{ textAlign: "center", color: "black", fontSize: 15, lineHeight: '22px' }}>
+              {activity.description.split("\n").map((line, i) => (
+                <span key={i}>
+                  {line}
+                  <br />
+                </span>
+              ))}
+            </div>
+
+            {/* 해시태그 */}
+            <div style={{ display: "flex", gap: 8, flexWrap: "wrap", justifyContent: "center" }}>
+              {activity.tags.map((tag, i) => (
                 <div
+                  key={i}
                   style={{
-                    color: "black",
-                    fontSize: 10,
-                    fontFamily: "Pretendard",
-                    fontWeight: 600,
+                    borderRadius: 4,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    padding: "2px 6px",
                   }}
                 >
-                  #{tag}
+                  <div
+                    style={{
+                      color: "black",
+                      fontSize: 10,
+                      fontFamily: "Pretendard",
+                      fontWeight: 600,
+                    }}
+                  >
+                    #{tag}
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+      );
 }
