03a43f9756256581a9c73e5dc66d0ef410112074
songye38 <ddev0308@gmail.com>
2025-12-01 18:42:43 +0900

    UPDATE func 모듈 임포트 수정

--- backend/app/routers/activity.py
+++ backend/app/routers/activity.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends,HTTPException,Query
-from sqlalchemy.orm import Session,func
+from sqlalchemy.orm import Session
+from sqlalchemy import func
 from typing import List
 from uuid import UUID
 import random
